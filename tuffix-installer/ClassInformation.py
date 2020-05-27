7bffee1c25fda022b447ae5faf525a57c9fbe19b
JaredDyreson <jared.dyreson@gmail.com>
2020-05-26 23:47:49 -0700

    cleaned things up and migrated the class info manifest to separate file

---
+++ tuffix-installer/ClassInformation.py
@@ -0,0 +1,7 @@
+"""
+This contains all the information about the computer science classes
+"""
+
+class_information_map = {
+    "cpsc-120": "Introduction to C++"
+}
--- tuffix-installer/ast.py
+++ tuffix-installer/ast.py
@@ -1,37 +1,30 @@
 from rply.token import BaseBox
-
-info_map = {
-  "cpsc-120": "Introduction to C++"
-}
+from ClassInformation import class_information_map
 
 class Target(BaseBox):
-    def __init__(self, value):
-        self.value = value
-
-    def eval(self):
-        print("Target: {}".format(self.value))
-        return self.value
-
-class Op(BaseBox):
     def __init__(self, target):
         self.target = target
 
-class Install(Op):
+    def eval(self):
+        print("Target: {}".format(self.target))
+        return self.target
+
+class Install(Target):
     def eval(self):
       print("[+] Installing {}".format(self.target))
 
-class Remove(Op):
+class Remove(Target):
     def eval(self):
       print("[+] Removing {}".format(self.target))
 
-class Describe(Op):
+class Describe(Target):
     def eval(self):
       try:
           print("[+] Information about {}".format(self.target))
-          print(info_map[self.target.lower()])
+          print(class_information_map[self.target.lower()])
       except KeyError as error:
           print("[-] Cannot retrieve information about {}".format(self.target))
 
-class Ignore(Op):
+class Ignore(Target):
     def eval(self):
         pass
